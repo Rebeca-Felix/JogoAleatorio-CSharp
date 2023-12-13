95e38cc21743f59ae6b7a1fedf0052ae491f6651
Rebeca Felix <97916363+Rebeca-Felix@users.noreply.github.com>
2023-12-13 18:51:17 -0300

    Add files via upload

---
+++ NumerosAleatórios/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+
+        int numeroAleatorio = random.Next(1, 101);
+        int tentativas = 0;
+        int palpite;
+
+        Console.WriteLine("Tente adivinhar o número entre 1 e 100:");
+
+        do
+        {
+            Console.Write("Palpite: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out palpite))
+            {
+                tentativas++;
+
+               if (palpite == numeroAleatorio)
+                {
+                    Console.WriteLine($"Parabéns! Você acertou em {tentativas} tentativas.");
+                }
+                else
+                {
+
+                    if (palpite < numeroAleatorio)
+                    {
+                        Console.WriteLine("Tente um número mais alto.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tente um número mais baixo.");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Por favor, insira um número válido.");
+            }
+
+        } while (palpite != numeroAleatorio);
+    }
+}
